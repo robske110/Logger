dd37e73f110f92d3c81bbd113c0dc63b4cdef4ce
Tim (robske_110) <robske110@users.noreply.github.com>
2022-01-13 19:24:48 +0100

    Improve PSRLogger
    Now it should even be relatively to-spec
    (Except for some limitations imposed by the legacy Logger)

--- src/PSRLogger.php
+++ src/PSRLogger.php
@@ -3,52 +3,54 @@ declare(strict_types=1);
 
 namespace robske_110\Logger;
 
+use Psr\Log\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 class PSRLogger implements LoggerInterface{
 	
-	public function emergency($message, array $context = array()): void{
+	public function emergency($message, array $context = []): void{
 		$this->log("emergency", $message);
 	}
 	
-	public function alert($message, array $context = array()): void{
+	public function alert($message, array $context = []): void{
 		$this->log("alert", $message);
 	}
 	
-	public function critical($message, array $context = array()): void{
+	public function critical($message, array $context = []): void{
 		$this->log("critical", $message);
 	}
 	
-	public function error($message, array $context = array()): void{
+	public function error($message, array $context = []): void{
 		$this->log("error", $message);
 	}
 	
-	public function warning($message, array $context = array()): void{
+	public function warning($message, array $context = []): void{
 		$this->log("warning", $message);
 	}
 	
-	public function notice($message, array $context = array()): void{
+	public function notice($message, array $context = []): void{
 		$this->log("notice", $message);
 	}
 	
-	public function info($message, array $context = array()): void{
+	public function info($message, array $context = []): void{
 		$this->log("info", $message);
 	}
 	
-	public function debug($message, array $context = array()): void{
+	public function debug($message, array $context = []): void{
 		$this->log("debug", $message);
 	}
 	
-	public function log($level, $message, array $context = array()): void{
+	public function log($level, $message, array $context = []): void{
 		$level = match($level){
-			"emergency", "alert" => Logger::LOG_LVL_EMERGENCY,
-			"critical", "error" => Logger::LOG_LVL_CRITICAL,
-			"warning" => Logger::LOG_LVL_WARNING,
-			"notice" => Logger::LOG_LVL_NOTICE,
-			"info" => Logger::LOG_LVL_INFO,
-			"debug" => Logger::LOG_LVL_DEBUG,
-			default => Logger::LOG_LVL_NOTICE,
+			LogLevel::EMERGENCY, LogLevel::ALERT => Logger::LOG_LVL_EMERGENCY,
+			LogLevel::CRITICAL, LOgLevel::ERROR => Logger::LOG_LVL_CRITICAL,
+			LogLevel::WARNING => Logger::LOG_LVL_WARNING,
+			LogLevel::NOTICE => Logger::LOG_LVL_NOTICE,
+			LogLevel::INFO => Logger::LOG_LVL_INFO,
+			LogLevel::DEBUG => Logger::LOG_LVL_DEBUG,
+			default => throw new InvalidArgumentException,
 		};
-		Logger::log($message, $level);
+		Logger::log((string) $message, $level);
 	}
 }
