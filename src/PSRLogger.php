79ed44d6f34fda0799326346a4531287f326170d
Tim (robske_110) <robske110@users.noreply.github.com>
2022-01-15 19:08:12 +0100

    PSRLogger: don't forget to pass context to log function

--- src/PSRLogger.php
+++ src/PSRLogger.php
@@ -10,35 +10,35 @@ use Psr\Log\LogLevel;
 class PSRLogger implements LoggerInterface{
 	
 	public function emergency($message, array $context = []): void{
-		$this->log("emergency", $message);
+		$this->log("emergency", $message, $context);
 	}
 	
 	public function alert($message, array $context = []): void{
-		$this->log("alert", $message);
+		$this->log("alert", $message, $context);
 	}
 	
 	public function critical($message, array $context = []): void{
-		$this->log("critical", $message);
+		$this->log("critical", $message, $context);
 	}
 	
 	public function error($message, array $context = []): void{
-		$this->log("error", $message);
+		$this->log("error", $message, $context);
 	}
 	
 	public function warning($message, array $context = []): void{
-		$this->log("warning", $message);
+		$this->log("warning", $message, $context);
 	}
 	
 	public function notice($message, array $context = []): void{
-		$this->log("notice", $message);
+		$this->log("notice", $message, $context);
 	}
 	
 	public function info($message, array $context = []): void{
-		$this->log("info", $message);
+		$this->log("info", $message, $context);
 	}
 	
 	public function debug($message, array $context = []): void{
-		$this->log("debug", $message);
+		$this->log("debug", $message, $context);
 	}
 	
 	public function log($level, $message, array $context = []): void{
